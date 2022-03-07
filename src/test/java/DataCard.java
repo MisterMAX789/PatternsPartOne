a34c32a4a2f9226fb80d39203a6bf097ef48d5ff
MisterMAX789 <maximotruc@mail.ru>
2022-03-07 23:25:52 +0300

    first commit

--- src/test/java/DataCard.java
+++
@@ -1,9 +0,0 @@
-import lombok.Data;
-
-@Data
-public class DataCard {
-    private final String cityYour;
-    private final String nameSurname;
-    private final String phoneNumber;
-
-}
--- src/test/java/DeliveryCard.java
+++ src/test/java/DeliveryCard.java
@@ -1,29 +1,62 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class DeliveryCard {
+
+  @BeforeEach
+  void setUp() {
+      open("http://localhost:9999/");
+      Configuration.holdBrowserOpen = true;
+      $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.DELETE);
+  }
+
     @Test
-    void shouldCompletedApplication() {
-        open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue("Курск");  //выбор города
-        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
-        String verificationDate = LocalDate.now().plusDays(5) //+5 дней к сегодняшней дате
-                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy")); // формат даты
-        $("[data-test-id=date] input").setValue(verificationDate);
-        $("[data-test-id=name] input").setValue("Шилов Николай");
-        $("[data-test-id=phone] input").setValue("+79997257198");
-        $("[data-test-id=agreement]").click();
-        $(".button").shouldHave(Condition.text("Запланировать")).click();
-        $("[data-test-id=notification]")
-                .shouldHave(Condition.exactText("Успешно! Встреча успешно забронирована на " + verificationDate),
-                        Duration.ofSeconds(15)); //Условие загрузки не более 15 секунд
+    public void shouldCompletedApplication() {
+        Faker faker = new Faker(new Locale("Ru"));
+        String verificationDate = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[data-test-id='city'] input").val(faker.address().cityName());
+        $("[data-test-id='date'] input").sendKeys(verificationDate);
+        $("[data-test-id='name'] input").val(faker.name().fullName());
+        $("[data-test-id='phone'] input").val(faker.number().digits(11));
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='success-notification']").shouldHave(text("Встреча успешно запланирована на " + verificationDate));
+        Duration.ofSeconds(15);
+    }
+
+    @Test
+    public void shouldChangeDate() {
+        Faker faker = new Faker(new Locale("Ru"));
+        String verification = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[data-test-id='city'] input").val(faker.address().cityName());
+        $("[data-test-id='date'] input").sendKeys(verification);
+        $("[data-test-id='name'] input").val(faker.name().fullName());
+        $("[data-test-id='phone'] input").val(faker.number().digits(11));
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='success-notification']").shouldHave(text("Встреча успешно запланирована на " + verification));
+        $(".button").click();
+        $("[data-test-id='replan-notification']").shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+        $(byText("Перепланировать")).click();
+        $("[data-test-id='success-notification']").shouldHave(text("Встреча успешно запланирована на " + verification));
+        Duration.ofSeconds(15);
     }
 }
+
+
+
